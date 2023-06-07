c3d35a1e77a9cb3032a95f9ba999ec4548d32bd0
Artem Nikitin <a.nikitin@4payments.com>
2023-06-07 16:29:43 +0500

    pizza chapter 2

---
+++ frontend/src/modules/constructor/SauceSelection.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="ingredients__sauce">
+        <p>Основной соус:</p>
+        <label
+            class="radio ingredients__input"
+            v-for="sauceType in sauceTypes"
+            :key="sauceType.id"
+        >
+            <input
+                type="radio"
+                name="sauce"
+                :value="sauceType.value"
+                :checked="sauceType.value === modelValue"
+                @input="emit('update:modelValue', sauceType.value)"
+            />
+            <span>{{ sauceType.name }}</span>
+        </label>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    modelValue: {
+        type: String,
+        default: "",
+    },
+    sauceTypes: {
+        type: Array,
+        default: () => [],
+    },
+});
+
+const emit = defineEmits(['update:modelValue']);
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds.scss";
+
+.ingredients__sauce {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+  margin-bottom: 14px;
+
+  p {
+    @include r-s16-h19;
+    margin-top: 0;
+    margin-right: 16px;
+    margin-bottom: 10px;
+  }
+}
+
+.ingredients__input {
+  margin-right: 24px;
+  margin-bottom: 10px;
+}
+</style>
